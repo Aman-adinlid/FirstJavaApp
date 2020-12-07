254742e7723506a928a1a5c288a1f5c9a648ca6f
Aman <95pinkpanda@gmail.com>
2020-12-07 15:05:31 +0100

    my first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,69 @@
+
+// Aman
+package com.company;
+
+public class Main {
+
+   public static void main(String[] args) {
+
+      System.out.println(1);
+      System.out.println(5.12);
+      System.out.println(10);
+
+      System.out.println(true);
+      System.out.println(false);
+      System.out.println("k");
+      System.out.println("hello");
+      System.out.println("Aman");
+
+      int pages = 6;
+      System.out.println(pages);
+      double weight = 2.3;
+      System.out.println(weight);
+      boolean ismoving= (true);
+      System.out.println(true);
+
+
+
+      int number =3+1;
+      System.out.println(number);
+      int number1 =2-3;
+      System.out.println(number1);
+      int number2 =3*4;
+      System.out.println(number2);
+      int number3 =4/2;
+      System.out.println(number3);
+
+      String myString = "Adam";
+      System.out.println(myString);
+      String myString1 = "stay strong";
+      System.out.println(myString1);
+      String myString2 = "u can do it";
+      System.out.println(myString2);
+
+
+
+
+
+
+
+
+   }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
